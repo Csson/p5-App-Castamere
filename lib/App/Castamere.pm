fc09d8cbaf5a2689071f70fc8cb6882921e6e579
Erik Carlsson <info@code301.com>
2016-09-13 23:27:38 +0000

    Init

---
+++ lib/App/Castamere.pm
@@ -0,0 +1,29 @@
+use 5.10.0;
+use strict;
+use warnings;
+
+package App::Castamere;
+
+# ABSTRACT: Short intro
+# AUTHORITY
+our $VERSION = '0.0100';
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use App::Castamere;
+
+=head1 DESCRIPTION
+
+App::Castamere is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+
+use App::Castamere;
+ok 1, 'Loaded';
+
+done_testing;
