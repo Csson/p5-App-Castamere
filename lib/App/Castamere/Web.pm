536d433ef72a3c94311f6b2be31532df824da5a1
Erik Carlsson <info@code301.com>
2016-09-18 13:34:18 +0000

    Add routes to svg, home and sub.

--- lib/App/Castamere/Web.pm
+++ lib/App/Castamere/Web.pm
@@ -29,13 +29,39 @@ sub startup($self) {
     $self->defaults(layout => 'default');
 
     $self->renderer->cache->max_keys(0);
+
     my $r = $self->routes;
  #   $r->cache->max_keys(0);
-    $r->get('/')->to(cb => sub($c) {
-        my($file) = grep { $_->fileinfo->filename =~ m{Sub/Quote\.pm} } $self->reporter->files->@*;
-        $c->render(template => 'views/one_source_file', file => $file);
-    });
 
+
+    $r->get('/file/*filename')->to(cb => sub($c) {
+        my($file) = grep { $_->fileinfo->filename eq $c->param('filename') } $self->reporter->files->@*;
+        $c->render(template => 'views/one_source_file', file => $file);
+    })->name('one_file');
+
+    $r->get('/sub/*subname')->to(cb => sub($c) {
+        my @file_line_range = $self->reporter->profile->file_line_range_of_sub($c->param('subname'));
+        my $filename = $file_line_range[0];
+        my $first_line = $file_line_range[2] || -1;
+        my $last_line = $file_line_range[3] || -1;
+
+        if(defined $filename) {
+            my($file) = grep { $_->fileinfo->filename eq $file_line_range[0] } $self->reporter->files->@*;
+            $c->stash(start_at => $first_line);
+            $c->stash(end_at => $last_line);
+            $c->render(layout => undef, template => 'views/one_source_file', file => $file);
+        }
+        else {
+            $c->render(text => 'No source to show.');
+        }
+    })->name('sub');
+
+    $r->get('/svg')->to(cb => sub ($c) {
+        $c->render(template => 'views/svg', reporter => $self->reporter);
+    })->name('svg');
+    $r->get('/')->to(cb => sub ($c) {
+        $c->render(template => 'views/overview', reporter => $self->reporter);
+    });
 }
 
 1;
