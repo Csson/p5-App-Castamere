52ee9ae165b754ae7ccadb912c3a3bf5ec527424
Erik Carlsson <info@code301.com>
2016-09-19 07:19:47 +0000

    Adapted to the new id's and classes of Graph::Flames which makes zooming quite a bit faster by being aware of the stacks. Especially search highlighting is faster.

--- share/public/resources/app-proffy-svg.js
+++ share/public/resources/app-proffy-svg.js
@@ -2,18 +2,25 @@ var pressedKeys = {
     shift: false,
     ctrl: false,
 };
+var ticksPerSecond = 1;
 
 $(document).ready(function() {
     var $svg = $('#svg > svg:first');
     var svgHeight = $svg.attr('height');
+    var svgWidth = $svg.attr('width');
     var $searchResultContainer = $svg.find('#search-results');
-    var $baseChainText = $svg.find('g.chain:first').find('text:first');
+    var $baseChainText = $svg.find('g#chains > g:first').find('text:first');
+    ticksPerSecond = cleanNumber($svg.data('ticks-per-second'));
     var $clickedGroup;
 
-    $svg.find('g.chain')
+    $svg.find('g#chains > g')
         .mouseover(function(e) {
-            var $rect = $(this);
-            $('#hovered-sub').text($rect.find('title:first').text());
+            var $group = $(this);
+            if(!$group.find('title').size()) {
+                var fullName = getGroupTitle($group, 1);
+            }
+            $('#hovered-sub').text($group.find('title:first').text());
+
         })
         .mouseout(function(e) {
             $('.extra-hovered').removeClass('extra-hovered');
@@ -29,7 +36,7 @@ $(document).ready(function() {
             var svgWidth = $svg.attr('width');
             var fontWidth = $svg.data('font-width');
 
-            var subName = $group.data('name');
+            var subName = getGroupTitle($group);
 
             // if the bottom bar is clicked -> nothing to fetch
             if(subName) {
@@ -59,56 +66,55 @@ $(document).ready(function() {
             $('.zoomed').removeClass('zoomed');
             $('.zoom-ancestor').removeClass('zoom-ancestor');
             $('.zoom-descendant').removeClass('zoom-descendant');
-            $('.zoom-unwanted').removeClass('zoom-unwanted');
             $('.zoom-too-thin').removeClass('zoom-too-thin');
+
+            // Most blobs are likely to be hidden so hide all first (but special-case the black bar)
+            if($group.attr('id') !== 's-1') {
+                $svg.find('g#chains > g').addClass('zoom-unwanted');
+            }
 
             var oldOrigin = parseFloat($rect.data('orig-x') || $rect.attr('x'));
             var oldWidth = parseFloat($rect.data('orig-width') || $rect.attr('width'));
             var oldY = parseFloat($rect.attr('y'));
 
             changeChainAppearance($group, 0, svgWidth, fontWidth);
-            $group.addClass('zoomed');
+            $group.addClass('zoomed').removeClass('zoom-unwanted');
 
-            $svg.find('g.chain:not(.zoomed)').each(function() {
+            // Find all descendants and make them a bit wider
+            var wantedClass = $group.attr('id');
+            $svg.find('g#chains > g.' + wantedClass).each(function() {
                 var $foundGroup = $(this);
 
                 var $foundRect = $foundGroup.find('rect:first');
-                var $foundText = $foundGroup.find('text:first');
-                var $foundBorderRight = $foundGroup.find('line.border-right:first');
+                var $foundBorderRight = $foundGroup.find('line:first-of-type');
 
                 var foundX = parseFloat($foundRect.data('orig-x') || $foundRect.attr('x'));
                 var foundWidth = parseFloat($foundRect.data('orig-width') || $foundRect.attr('width'));
                 var foundY = parseFloat($foundRect.attr('y'));
 
-                var isAbove = foundY < oldY;
-                var isBelow = foundY > oldY;
+                var newX = floatRound(svgWidth * ((foundX - oldOrigin) / oldWidth), 3);
 
-                var isDescendant = isAbove
-                              && foundX >= oldOrigin                         // starts at or right of the start of the clicked area
-                              && foundX < oldOrigin + oldWidth;              // starts left of the end of the clicked area
-                var isAncestor = isBelow
-                              && foundX < oldOrigin + oldWidth               // starts left of the end of the clicked area
-                              && foundX + foundWidth > oldOrigin;            // ends right of the start of the clicked area
-
-                if(!isAncestor && !isDescendant) {
-                    $foundGroup.addClass('zoom-unwanted');
-                    return;
-                }
-                else {
-
-                    if(isAncestor) {
-                        changeChainAppearance($foundGroup, 0, svgWidth, fontWidth);
-                        $foundGroup.addClass('zoom-ancestor');
-                    }
-                    else if(isDescendant) {
-                        var newX = floatRound(svgWidth * ((foundX - oldOrigin) / oldWidth), 3);
-
-                        var newWidth = floatRound(svgWidth * (foundWidth / oldWidth), 3);
-                        changeChainAppearance($foundGroup, newX, newWidth, fontWidth);
-                        $foundGroup.addClass('zoom-descendant');
-                    }
-                }
+                var newWidth = floatRound(svgWidth * (foundWidth / oldWidth), 3);
+                changeChainAppearance($foundGroup, newX, newWidth, fontWidth);
+                $foundGroup.addClass('zoom-descendant').removeClass('zoom-unwanted');
             });
+
+            // make a '#list, #of, #ids' from classes starting with 's-'
+            var ancestorIds = $group.attr('class')
+                                    .split(/ +/)
+                                    .filter((v) => 's-' === v.substring(0, 2))
+                                    .map((v) => '#' + v)
+                                    .join(', ');
+
+            // Find all ancestors, but there are no ancestors for #s-1
+            if($group.attr('id') !== 's-1') {
+                $svg.find('g#chains > g').filter(ancestorIds).each(function() {
+                    var $foundGroup = $(this);
+                    changeChainAppearance($foundGroup, 0, svgWidth, fontWidth);
+                    $foundGroup.addClass('zoom-ancestor').removeClass('zoom-unwanted');
+                });
+            }
+
             updateBaseChainText($group);
             $('#search-form').submit();
         });
@@ -134,60 +140,48 @@ $(document).ready(function() {
             searchRegex = new RegExp(searchString);
         }
         catch(e) {
-            console.log("'" + searchString + "' is not a regex");
         }
         if(searchRegex) {
             var count = 0;
             var percent = 0;
             var checkCount = 0;
             var microSeconds = 0;
+            var searchedUntilX = svgWidth;
 
-            $svg.find('g.chain:not(.zoom-unwanted)').each(function() {
+            $svg.find('g#chains > g:not(.zoom-unwanted)').each(function() {
                 var $group = $(this);
                 var $title = $group.find('title:first');
                 var $rect = $group.find('rect:first');
 
-                // possibly invert search result
-                var searchMatches = $group.data('name').match(searchRegex);
+                // We have already found what we are searching for right of here
+                if(parseFloat($rect.attr('x')) >= searchedUntilX) {
+                    return true;
+                }
+                var fullName = getGroupTitle($group, 0);
+                var searchMatches = fullName.match(searchRegex);
 
                 if(searchMatches) {
                     $group.addClass('search-result');
 
                     var matchX = parseFloat($rect.attr('x'));
                     var matchWidth = parseFloat($rect.attr('width'));
-
-                    var hasOverlap = 0;
-                    // Find already existing search result highlighters so that
-                    // we don't create unnecessary <rect>s and can
-                    // calculate correct percentage-
-                    $searchResultContainer.find('rect.search-highlighter, rect.search-highlighter-unwanted').each(function() {
-
-                        var $highlighted = $(this);
+                    searchedUntilX = matchX;
  
-                        if(    parseFloat($highlighted.attr('x')) <= matchX
-                            && parseFloat($highlighted.attr('x')) + parseFloat($highlighted.attr('width')) >= matchX + matchWidth) {
-                            hasOverlap++;
-                            return false;
-                        }
-                    });
-
-                    count++;
-                    var percentText = $group.data('percent');
-                    if(!hasOverlap && undefined !== percentText) {
+                    var percentText = $group.data('pc');
+                    if(undefined !== percentText) {
                         percent += parseFloat(percentText);
                         microSeconds += cleanNumber($group.data('ms'));
                     }
-                    if(!hasOverlap) {
-                        var highligherClass = negativeSearch ? 'search-highlighter-unwanted' : 'search-highlighter';
-                        var x = $rect.attr('width') < 1 ? $rect.attr('x') - 0.5 : $rect.attr('x');
-                        var width = $rect.attr('width') < 1 ? 1 : $rect.attr('width');
 
-                        addSearchHighlighter({
-                            x: x,
-                            width: width,
-                            class: highligherClass,
-                        });
-                    }
+                    var highligherClass = negativeSearch ? 'search-highlighter-unwanted' : 'search-highlighter';
+                    var x = $rect.attr('width') < 1 ? $rect.attr('x') - 0.5 : $rect.attr('x');
+                    var width = $rect.attr('width') < 1 ? 1 : $rect.attr('width');
+
+                    addSearchHighlighter({
+                        x: x,
+                        width: width,
+                        class: highligherClass,
+                    });
                 }
             });
 
@@ -203,7 +197,7 @@ $(document).ready(function() {
                     var highlightEnd = highlightX + highlightWidth;
                     allUnwanted.push({ x: highlightX, end: highlightEnd});
 
-                    if(highlightEnd < previousX) {
+          //          if(highlightEnd < previousX) {
                         var negativeHighlighter = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
                         var distanceBetween = previousX - highlightEnd;
 
@@ -215,7 +209,7 @@ $(document).ready(function() {
                             width: width,
                             class: 'search-highlighter',
                         });
-                    }
+             //       }
                     previousX = highlightX;
                 });
                 if(previousX > 0) {
@@ -261,7 +255,7 @@ $(document).ready(function() {
             $infoGroup = $baseChainText.parent();
         }
         var msAsNum = parseFloat(cleanNumber($infoGroup.data('ms')));
-        var newText = 'showing: ' + $infoGroup.data('ms')+'ms (' + $infoGroup.data('percent') + '%)';
+        var newText = 'showing: ' + $infoGroup.data('ms')+'ms (' + $infoGroup.data('pc') + '%)';
         if(undefined !== microSeconds) {
             var searchPercentOfShown = floatRound(100 * cleanNumber(microSeconds) / msAsNum, 2);
             microSeconds = formatNumber(microSeconds);
@@ -281,6 +275,54 @@ $(document).ready(function() {
         $searchResultContainer.get(0).appendChild(highlighter);
     }
 });
+function getGroupTitle($group, createElement) {
+    var fullName = $group.data('name');
+
+    if(fullName && !createElement) {
+        return fullName;
+    }
+
+    // Build the name from possibly referred data
+    // rns, ns | rsn, sn | rfn, fn | num
+    var allData = $group.data();
+
+    fullName = '';
+    fullName = fullName + (allData.rns ? allData.rns : allData.ns  ? $('#' + allData.ns).data('rns') : '');
+
+    // no use continuing without a name
+    if(!fullName.length) {
+        return '';
+    }
+    fullName = fullName + '::';
+    fullName = fullName + (allData.rsn ? allData.rsn : allData.sn  ? $('#' + allData.sn).data('rsn') : '');
+
+    if((allData.rfn || allData.fn) && allData.num) {
+        fullName = fullName + '__ANON__[';
+        fullName = fullName + (allData.rfn ? allData.rfn : allData.fn  ? $('#' + allData.fn).data('rfn') : '');
+        fullName = fullName + allData.num + ']';
+    }
+
+    $group.data('name', fullName);
+
+    if(createElement) {
+        if(!$group.find('text:first').size()) {
+            var textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            textEl.setAttribute('y', parseFloat($group.find('rect:first').attr('y')) + 13);
+            $group.get(0).appendChild(textEl);
+        }
+        if(!$group.find('title:first').size()) {
+            var titleEl = document.createElementNS('http://www.w3.org/2000/svg', 'title');
+            var percent = allData.pc;
+            var ticks = cleanNumber(allData.t);
+
+            var ms = formatNumber(floatRound(ticks / ticksPerSecond * 1000000, 0));
+            var titleText = document.createTextNode(fullName + ' (' + ms + 'ms, ' + percent + '%)');
+            titleEl.appendChild(titleText);
+            $group.get(0).appendChild(titleEl);
+        }
+    }
+    return fullName;
+}
 function formatNumber(number) {
     return ('' + number).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
@@ -307,7 +349,7 @@ function floatRound(number, precision) {
 function changeChainAppearance($group, newX, newWidth, fontWidth) {
     var $rect = $group.find('rect:first');
     var $text = $group.find('text:first');
-    var $borderRight = $group.find('line.border-right:first');
+    var $borderRight = $group.find('line:first-of-type');
 
     if(!$rect.data('orig-x')) {
         $rect.attr('data-orig-x', $rect.attr('x'));
@@ -319,9 +361,19 @@ function changeChainAppearance($group, newX, newWidth, fontWidth) {
     $borderRight.attr('x1', newX + newWidth)
                 .attr('x2', newX + newWidth);
 
-    $text.attr('x', newX + 2.5);
-    $text.text(longestPossibleText($group.data('name'), $rect.attr('width'), fontWidth));
+    var title = longestPossibleText(getGroupTitle($group, 0), $rect.attr('width'), fontWidth);
 
+    if(title) {
+        if(!$group.find('text:first').size()) {
+            getGroupTitle($group, 1);
+        }
+        $text = $group.find('text:first');
+        $text.attr('x', newX + 2.5);
+        $text.text(title);
+    }
+    else if($text.size()) {
+        $text.text('');
+    }
     if($rect.attr('width') < 0.3) { $group.addClass('zoom-too-thin'); }
 
     $group.addClass('zoom-descendant');
